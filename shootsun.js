e52b6c1ff4125d9b430c0784eb499856c2def3a6
samantha lin <samanthalin1992@gmail.com>
2018-04-17 03:57:32 +0800

    explode

--- shootsun.js
+++ shootsun.js
@@ -1,9 +1,14 @@
 AFRAME.registerComponent("sun-shooting",{
   init : function(){
-    suns = document.querySelector(".sun")
+    suns = document.querySelectorAll(".sun")
     suns.forEach(function(sun){
-     sun.addEventListener("click",function(){
-      this.setAttribute("visible",false)
-    })
-  }
+    	sun.addEventListener("click",function(){
+    		if(this.id == "real-sun"){
+    			alert("You shot the real sun.")
+    		}else{
+    			this.emit("explode");
+    		}
+    	})
+  	})
+	}
 })
