f69d077ed2b3757d08688e7051deb4e606ddb5fb
samantha lin <samanthalin1992@gmail.com>
2018-04-17 02:57:44 +0800

    js

---
+++ shootsun.js
@@ -0,0 +1,8 @@
+AFRAME.registerComponent("sun-shooting",{
+  init : function(){
+    suns = document.querySelector(".sun")
+    suns.forEach(function(sun){
+     sun.addEventListener("click",function(){
+    })
+  }
+})
