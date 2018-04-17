a2d5178f98e80df5d96bdf663205761f17067f02
samantha lin <samanthalin1992@gmail.com>
2018-04-17 12:23:34 +0800

    game done

--- shootsun.js
+++ shootsun.js
@@ -1,14 +1,24 @@
+var totalSunShot = 0;
 AFRAME.registerComponent("sun-shooting",{
   init : function(){
     suns = document.querySelectorAll(".sun")
     suns.forEach(function(sun){
-    	sun.addEventListener("click",function(){
-    		if(this.id == "real-sun"){
-    			alert("You shot the real sun.")
-    		}else{
-    			this.emit("explode");
-    		}
-    	})
-  	})
-	}
+        sun.addEventListener("click",function(){
+            if(this.id == "real-sun"){
+                var text = document.querySelector('#real-text');
+                text.setAttribute("visible", true);
+            }else{
+                this.emit("explode");
+                totalSunShot++;
+                var scoreboard =  document.getElementById("scoreboard");
+                scoreboard.setAttribute("value", (5 - totalSunShot) + "/ 5 Left");
+                if(totalSunShot == 5){
+                    var gameover = document.getElementById("game-over");
+                    gameover.setAttribute("visible", true);
+                    // goto next page here
+                }
+            }
+        })
+    })
+    }
 })
